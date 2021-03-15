3bd0c50fe2bd9ea6507246d2e50d0ff716b18cbb
Pawan kumar <60340211+gitcoder-pawan@users.noreply.github.com>
2021-03-15 19:50:51 +0530

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+ int main (){
+ 	int n;
+ 	cout<<"enter no of names: "<<endl;
+ 	cin>>n;
+ 	cout<<"enter names "<<endl;
+ 	string s1, s2, s3, s4;
+ 	cin>>s1;
+ 	cin>>s2;
+ 	cin>>s3;
+ 	cin>>s4;
+ 	
+ 	if (s1=="prince") cout<<"yes";
+ 	else if (s2=="prince") cout<<"yes";
+ 	else if (s3=="prince") cout<<"yes";
+ 	else if (s4=="prince") cout<<"yes";
+ 	else cout<<"no";
+ }
